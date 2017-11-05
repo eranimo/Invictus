3e7f686f1022d8410da11bde01e5a222a3d04a38
Kaelan Cooter <dcgamers@gmail.com>
2017-11-05 18:22:01 -0500

    basic goap planning

--- packages/planner/src/__tests__/agent.spec.ts
+++ packages/planner/src/__tests__/agent.spec.ts
@@ -1,11 +1,110 @@
-import Agent from '../index';
+import { Agent, Plan, Action, Goal } from '../index';
+
+type State = {
+  hasAxe: boolean,
+  hasLog: boolean,
+  canGetAxe: boolean,
+};
 
 
-let agent: Agent;
-beforeEach(() => {
-  agent = new Agent();
-});
+// actions
+class ChopLog implements Action<State> {
+  cost(): number {
+    return 4;
+  }
 
-test('Agent', () => {
-  expect(agent.actions.length).toBe(1);
+  precondition(state: State): boolean {
+    return !state.hasLog && state.hasAxe;
+  }
+
+  effect(state: State): State {
+    state.hasLog = true;
+    return state;
+  }
+}
+
+class GetAxe implements Action<State> {
+  cost(): number {
+    return 2;
+  }
+
+  precondition(state: State): boolean {
+    return !state.hasAxe && state.canGetAxe;
+  }
+
+  effect(state: State): State {
+    state.hasAxe = true;
+    return state;
+  }
+}
+
+class CollectBranches implements Action<State> {
+  cost(): number {
+    return 8;
+  }
+
+  precondition(state: State): boolean {
+    return !state.hasLog;
+  }
+
+  effect(state: State): any {
+    state.hasLog = true;
+    return state;
+  }
+}
+
+class MakeFirewoodGoal implements Goal<State> {
+  condition(state: State) {
+    return state.hasLog == true;
+  }
+}
+
+class Woodcutter extends Agent<State> {
+  constructor(state: State) {
+    super();
+    this.state = state;
+
+    this.addAction(new ChopLog());
+    this.addAction(new GetAxe());
+    this.addAction(new CollectBranches());
+  }
+
+  plan(): Plan<State> | null {
+    return Plan.formulate<State>(this, new MakeFirewoodGoal());
+  }
+}
+
+
+describe('Make firewood GOAP plan', () => {
+  it('best way to get logs without an axe', () => {
+    let woodcutter = new Woodcutter({
+      hasAxe: false,
+      hasLog: false,
+      canGetAxe: false,
+    });
+    expect(woodcutter.actions.size).toBe(3);
+
+    const plan = woodcutter.plan();
+
+    expect(plan).toBeDefined();
+    expect(plan.sequence).toHaveLength(1);
+    expect(plan.sequence[0]).toBeInstanceOf(CollectBranches);
+    expect(plan.totalCost).toBe(8);
+  });
+
+  it('best way to get logs with an axe', () => {
+    let woodcutter = new Woodcutter({
+      hasAxe: false,
+      hasLog: false,
+      canGetAxe: true,
+    });
+    expect(woodcutter.actions.size).toBe(3);
+
+    const plan = woodcutter.plan();
+
+    expect(plan).toBeDefined();
+    expect(plan.sequence).toHaveLength(2);
+    expect(plan.sequence[0]).toBeInstanceOf(GetAxe);
+    expect(plan.sequence[1]).toBeInstanceOf(ChopLog);
+  });
 });
--- packages/planner/src/index.ts
+++ packages/planner/src/index.ts
@@ -1,11 +1,130 @@
-export class Action {
+import createGraph from 'ngraph.graph';
+import graphPath from 'ngraph.path';
 
+
+const DEBUG = false;
+
+export interface Goal<T> {
+  // a Condition is a function that evaluates a state object
+  // and returns whether or not the state object meets a criteria
+  condition(state: T): boolean;
+
+  // a comparator is a function that takes the old state and the potential new state 
+  // and decides whether or not the new state should be accepted
+  comparator?(oldState: T, newState: T): boolean;
 }
 
-export default class Agent {
-  actions: Array<Action>;
+
+export interface Action<T> {
+  // a function that decides if the action can be ran
+  precondition(state: T): boolean;
+
+  // a function that mutates state after the action is executed
+  effect(state: T): T;
+
+  // a function that decides the cost of this action given the current state
+  // this might be constant
+  cost(state: T): number;
+}
+
+
+export class Plan<T> {
+  sequence: Array<Action<T>>;
+  totalCost: number;
+
+  constructor(sequence: Array<Action<T>>, totalCost: number) {
+    this.sequence = sequence;
+    this.totalCost = totalCost;
+  }
+
+  static formulate<T>(agent: Agent<T>, goal: Goal<T>): Plan<T> | null {
+    const graph = createGraph();
+
+    let currentID = 0;
+    const root = graph.addNode(currentID, {
+      cost: 0,
+      state: agent.state,
+    });
+
+    const actions = new Set(agent.actions);
+
+    let goalFound = true;
+    function buildGraph(node: any, actions): Node | boolean {
+      let foundOne = false;
+      for (const action of actions) {
+        const canRun = action.precondition(node.data.state) === true;
+        if (canRun) {
+          const newState = action.effect(Object.assign({}, node.data.state));
+          currentID++;
+          const reachedGoal = goal.condition(newState);
+          const newNode = graph.addNode(currentID, {
+            action,
+            cost: node.data.cost + action.cost(newState),
+            state: newState,
+            reachedGoal,
+          });
+          graph.addLink(node.id, newNode.id);
+
+          if (reachedGoal) {
+            if (DEBUG) console.log(`${action.constructor.name} - Reached Goal`);
+            // this action achieves the goal
+            foundOne = true;
+          }
+          if (DEBUG) console.log(`${action.constructor.name} - Continuing`);
+          // this action doesn't achieve the goal
+          // try another combination of actions
+          actions.delete(action); // remove action from list
+          const foundChild = buildGraph(newNode, actions);
+          if (foundChild) {
+            foundOne = true;
+          }
+        } else {
+          if (DEBUG) console.log(`${action.constructor.name} - Can NOT run`);
+        }
+      }
+      // could not find any actions that reach goal
+      return foundOne;
+    }
+
+    const result: any = buildGraph(root, actions);
+
+    // we have found at least one path to the goal
+    if (result) {
+      let endNodes = [];
+      graph.forEachNode(node => {
+        if (node.data.reachedGoal) {
+          endNodes.push(node);
+        }
+      });
+      const sortedEndNodes = endNodes.sort((a, b) => a.data.cost - b.data.cost);
+      if (DEBUG) console.log(sortedEndNodes);
+      const result = sortedEndNodes[0];
+      const pathFinder = graphPath.aStar(graph);
+      const path = pathFinder.find(root.id, result.id, {
+        distance(fromNode, toNode, link) {
+          return toNode.data.cost;
+        }
+      }).reverse().filter(node => node.data.action);
+      const sequence = path.map(node => node.data.action);
+      if (DEBUG) console.log(sequence)
+      const totalCost = path[0].data.cost;
+      return new Plan(sequence, totalCost);
+    }
+    return null;
+  }
+}
+
+export class Agent<T> {
+  actions: Set<Action<T>>;
+  state: Object;
+  currentPlan: Plan<T>;
 
   constructor() {
-    this.actions = [new Action()];
+    this.actions = new Set();
+    this.state = {};
+  }
+
+  addAction(action: Action<T>) {
+    this.actions.add(action);
   }
 }
