be489025249a3761549da9444b518580a93da1d0
Kaelan Cooter <dcgamers@gmail.com>
2017-11-19 19:42:57 -0500

    added Grid and Cell data structures

---
+++ packages/generator/src/__tests__/grid.spec.ts
@@ -0,0 +1,197 @@
+import ndarray from 'ndarray';
+import Grid from '../grid';
+
+
+const GRID_SIZE = 50;
+describe('Grid', () => {
+  interface ChunkGrid {
+    height: ndarray;
+  }
+  let grid: Grid<ChunkGrid>;
+
+  beforeEach(() => {
+    grid = new Grid<ChunkGrid>(GRID_SIZE, GRID_SIZE, {
+      height: ndarray([], [GRID_SIZE, GRID_SIZE]),
+    });
+  });
+
+  describe('basic operations', () => {
+    it('should allow field getting and setting', () => {
+      grid.setField(0, 0, 'height', 1);
+      expect(grid.getField(0, 0, 'height')).toBe(1);
+    });
+
+    it('cell toString', () => {
+      expect(grid.getCell(0, 0).toString()).toBe('Cell(x: 0, y: 0)');
+    });
+
+    it('cell toPrimitive', () => {
+      expect(+grid.getCell(0, 0)).toBe(0);
+      expect(+grid.getCell(10, 10)).toBe(510);
+      expect(`${grid.getCell(0, 0)}`).toBe('Cell(x: 0, y: 0)');
+    });
+
+    it('allows setting fields on the cell', () => {
+      const cell = grid.getCell(0, 0);
+      cell.data.height = 10;
+      expect(grid.getField(0, 0, 'height')).toBe(10);
+      cell.data.height = 11;
+      expect(grid.getField(0, 0, 'height')).toBe(11);
+      grid.setField(0, 0, 'height', 20);
+      expect(cell.data.height).toBe(20);
+    });
+
+    it('should allow lookup of field properties on Cell objects using getCell', () => {
+      grid.setField(0, 0, 'height', 1);
+      expect(grid.getCell(0, 0).data.height).toBe(1);
+      grid.setField(0, 0, 'height', 2);
+      expect(grid.getCell(0, 0).data.height).toBe(2);
+    });
+
+    it('should only instantiate one cell per coordinate', () => {
+      grid.setField(0, 0, 'height', 1);
+      grid.setField(0, 1, 'height', 1);
+      const cell1 = grid.getCell(0, 0);
+      const cell2 = grid.getCell(0, 1);
+      expect(cell1).toBe(grid.getCell(0, 0));
+      expect(cell2).toBe(grid.getCell(0, 1));
+      expect(cell1).not.toBe(cell2);
+    });
+
+    it('should instantiate when needed', () => {
+      expect(grid.getCell(20, 20).data.height).toBe(undefined);
+    })
+  });
+
+  it('distanceTo', () => {
+    const cell1 = grid.getCell(25, 25);
+    const cell2 = grid.getCell(25, 25);
+    const cell3 = grid.getCell(25, 30);
+    const cell4 = grid.getCell(35, 35);
+    expect(cell1.distanceTo(cell2)).toBe(0);
+    expect(cell1.distanceTo(cell3)).toBe(5);
+    expect(cell1.distanceTo(cell4)).toBe(10 * Math.sqrt(2));
+  });
+
+  describe('cell neighborhoods', () => {
+    it('cardinal direction methods', () => {
+      grid.setField(5, 5, 'height', 1);
+      const cell = grid.getCell(5, 5);
+
+      expect(cell.cellUp).toBe(grid.getCell(5, 4));
+      expect(cell.cellDown).toBe(grid.getCell(5, 6));
+      expect(cell.cellLeft).toBe(grid.getCell(4, 5));
+      expect(cell.cellRight).toBe(grid.getCell(6, 5));
+    });
+
+    describe('moore neighborhood', () => {
+      it('at radius 0', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getMooreNeighborhood(0);
+        expect(neighborhood).toHaveLength(1);
+        expect(neighborhood[0]).toBe(cell);
+      });
+      it('at radius 1', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getMooreNeighborhood(1);
+        expect(neighborhood).toHaveLength(9);
+        expect(neighborhood).toContain(grid.getCell(24, 24)); // top left
+        expect(neighborhood).toContain(grid.getCell(25, 25)); // center
+        expect(neighborhood).toContain(grid.getCell(26, 26)); // bottom right
+      });
+
+      it('at radius 5', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getMooreNeighborhood(5);
+        expect(neighborhood).toHaveLength(Math.pow(5 + 5 + 1, 2));
+        expect(neighborhood).toContain(grid.getCell(20, 20)); // top left
+        expect(neighborhood).toContain(grid.getCell(25, 25)); // center
+        expect(neighborhood).toContain(grid.getCell(30, 30)); // bottom right
+      });
+    });
+
+    describe('circular neighborhood', () => {
+      it('at radius 0', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getCircularNeighborhood(0);
+        expect(neighborhood).toHaveLength(1);
+        expect(neighborhood[0]).toBe(cell);
+      });
+
+      it('at radius 1', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getCircularNeighborhood(1);
+        expect(neighborhood).toHaveLength(5);
+        expect(neighborhood).toContain(grid.getCell(25 - 1, 25)); // top
+        expect(neighborhood).toContain(grid.getCell(25 + 1, 25)); // bottom
+        expect(neighborhood).toContain(grid.getCell(25, 25 - 1)); // left
+        expect(neighborhood).toContain(grid.getCell(25, 25 + 1)); // left
+        expect(neighborhood).toContain(grid.getCell(25, 25)); // center
+      });
+
+      it('at radius 5', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getCircularNeighborhood(5);
+        expect(neighborhood).toHaveLength(81);
+        expect(neighborhood).toContain(grid.getCell(25, 25 - 5)); // top edge
+        expect(neighborhood).toContain(grid.getCell(25, 25 + 5)); // bottom edge
+        expect(neighborhood).toContain(grid.getCell(25 - 5, 25)); // left edge
+        expect(neighborhood).toContain(grid.getCell(25 + 5, 25)); // right edge
+        // should not contain the corners (it's a circle)
+        expect(neighborhood).not.toContain(grid.getCell(25 - 5, 25 - 5)); // top left
+        expect(neighborhood).not.toContain(grid.getCell(25 - 5, 25 + 5)); // top right
+        expect(neighborhood).not.toContain(grid.getCell(25 - 5, 25 + 5)); // bottom left
+        expect(neighborhood).not.toContain(grid.getCell(25 + 5, 25 + 5)); // bottom right
+      })
+    });
+
+    describe('von neumann neighborhood', () => {
+      it('at radius 0', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getVonNeumannNeighborhood(0);
+        expect(neighborhood).toHaveLength(1);
+        expect(neighborhood[0]).toBe(cell);
+      });
+
+      it('at radius 1', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getVonNeumannNeighborhood(1);
+        expect(neighborhood).toHaveLength(2 * 1 * (1 + 1) + 1);
+        expect(neighborhood).toContain(grid.getCell(25 - 1, 25)); // top
+        expect(neighborhood).toContain(grid.getCell(25 + 1, 25)); // bottom
+        expect(neighborhood).toContain(grid.getCell(25, 25 - 1)); // left
+        expect(neighborhood).toContain(grid.getCell(25, 25 + 1)); // left
+        expect(neighborhood).toContain(grid.getCell(25, 25)); // center
+      });
+
+      it('at radius 5', () => {
+        const cell = grid.getCell(25, 25);
+        const neighborhood = cell.getVonNeumannNeighborhood(5);
+        expect(neighborhood).toHaveLength(2 * 5 * (5 + 1) + 1);
+        expect(neighborhood).toContain(grid.getCell(25, 25 - 5)); // top edge
+        expect(neighborhood).toContain(grid.getCell(25, 25 + 5)); // bottom edge
+        expect(neighborhood).toContain(grid.getCell(25 - 5, 25)); // left edge
+        expect(neighborhood).toContain(grid.getCell(25 + 5, 25)); // right edge
+      });
+    });
+
+    it('clearing neighborhood cache', () => {
+      const cell = grid.getCell(25, 25);
+      cell.getVonNeumannNeighborhood(0);
+      expect((cell.getVonNeumannNeighborhood as any).cache.has(0)).toBe(true);
+      expect((cell.getVonNeumannNeighborhood as any).cache.has(1)).toBe(false);
+      cell.clearCache();
+      expect((cell.getVonNeumannNeighborhood as any).cache.has(0)).toBe(false);
+    });
+  });
+
+  describe('cell search functions', () => {
+    it('von neumann search', () => {
+      const cell = grid.getCell(25, 25);
+      grid.setField(30, 41, 'height', 100);
+      const search = cell.vonNeumannSearch(cell => cell.data.height === 100, 50);
+      expect(search).not.toBe(null);
+      expect(search.data.height).toBe(100);
+    });
+  });
+});
---
+++ packages/generator/src/grid.ts
@@ -0,0 +1,255 @@
+import ndarray from 'ndarray';
+import { Graph } from 'ngraph.graph';
+import { memoize } from 'lodash';
+
+
+interface CellData { [fieldName: string]: any };
+interface INdarrayMap { [fieldName: string]: ndarray };
+
+export class Cell<Fields> {
+  x: number;
+  y: number;
+  grid: Grid<Fields>;
+  data: any;
+
+  getVonNeumannNeighborhood: (radius: number) => Array<Cell<Fields>>;
+  getMooreNeighborhood: (radius: number) => Array<Cell<Fields>>;
+  getCircularNeighborhood: (radius: number) => Array<Cell<Fields>>;
+
+  constructor(x: number, y: number, grid: Grid<Fields>) {
+    this.x = x;
+    this.y = y;
+    this.grid = grid;
+    this.data = {};
+
+    // add fields to the cell
+    for (const key in this.grid.fields) {
+      if (this.grid.fields.hasOwnProperty(key)) {
+        Object.defineProperty(this.data, key, {
+          enumerable: true,
+          configurable: false,
+          get: () => {
+            return this.grid.getField(x, y, key);
+          },
+          set: (value: any) => {
+            this.grid.setField(x, y, key, value);
+          }
+        });
+      }
+    }
+
+    this.getVonNeumannNeighborhood = memoize(this._getVonNeumannNeighborhood);
+    this.getMooreNeighborhood = memoize(this._getMooreNeighborhood);
+    this.getCircularNeighborhood = memoize(this._getCircularNeighborhood);
+  }
+
+  clearCache() {
+    (this.getVonNeumannNeighborhood as any).cache.clear();
+    (this.getMooreNeighborhood as any).cache.clear();
+    (this.getCircularNeighborhood as any).cache.clear();
+  }
+
+  get hash() {
+    return `${this.x},${this.y}`;
+  }
+
+  /**
+   * Gets all cells within a given radius of this cell
+   * using the von Neumann neighborhood (diamond shape)
+   * Returns an array of cells
+   * @param radius
+   */
+  _getVonNeumannNeighborhood(radius: number = 1): Array<Cell<Fields>> {
+    let result = []
+    if (radius === 0) return [this];
+    for (const cell of this.grid) {
+      if (Math.abs(cell.x - this.x) + Math.abs(cell.y - this.y) <= radius) {
+        result.push(cell);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Gets all cells within a given radius of this cell
+   * using the Moore neighborhood (square shape)
+   * @param radius 
+   */
+  _getMooreNeighborhood(radius: number = 1): Array<Cell<Fields>> {
+    let result = [];
+    if (radius === 0) return [this];
+    for (const cell of this.grid) {
+      if (
+        Math.abs(cell.x - this.x) <= radius &&
+        Math.abs(cell.y - this.y) <= radius
+      ) {
+        result.push(cell);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Gets all cells within a given radius of this cell
+   * in the form of a circle shape
+   * @param radius
+   */
+  _getCircularNeighborhood(radius: number = 1): Array<Cell<Fields>> {
+    let result = new Set();
+    if (radius === 0) return [this];
+    // loop over the top-left quadrant of the circle, add the points on the other 3
+    for (let x = this.x - radius; x <= this.x; x++) {
+      for (let y = this.y - radius; y <= this.y; y++) {
+        if (
+          Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2) <= Math.pow(radius, 2)
+        ) {
+          const xSim = this.x - (x - this.x);
+          const ySim = this.y - (y - this.y);
+          result.add(this.grid.getCell(x, y));
+          result.add(this.grid.getCell(x, ySim));
+          result.add(this.grid.getCell(xSim, y));
+          result.add(this.grid.getCell(xSim, ySim));
+        }
+      }
+    }
+    return Array.from(result);
+  }
+
+  /**
+   * Searches outwards from this cell in the moore neighborhood
+   * until a test function returns true
+   * @param cell how many rings
+   * @param limit how many rings to search until it gives up
+   * @returns Cell<Fields> or null if none found within limit
+   */
+  _search(
+    neighborhoodFunc: (radius: number) => Array<Cell<Fields>>,
+    testFunc: (cell: Cell<Fields>) => boolean,
+    limit: number = 50
+  ): Cell<Fields> | null {
+    let searchedCells = new Set();
+    let currentRadius = 0;
+
+    // expand radius until we're at the limit
+    while (currentRadius < limit) {
+      const cells = neighborhoodFunc(currentRadius).filter(cell => !searchedCells.has(cell));
+      for (const cell of cells) {
+        if (testFunc(cell)) {
+          return cell;
+        }
+        searchedCells.add(cell);
+      }
+      currentRadius++;
+    }
+    return null; // not found
+  }
+
+  mooreSearch(
+    testFunc: (cell: Cell<Fields>) => boolean,
+    limit: number = 50
+  ): Cell<Fields> | null {
+    return this._search(this.getMooreNeighborhood.bind(this), testFunc, limit);
+  }
+
+  vonNeumannSearch(
+    testFunc: (cell: Cell<Fields>) => boolean,
+    limit: number = 50
+  ): Cell<Fields> | null {
+    return this._search(this.getVonNeumannNeighborhood.bind(this), testFunc, limit);
+  }
+
+  circularSearch(
+    testFunc: (cell: Cell<Fields>) => boolean,
+    limit: number = 10
+  ): Cell<Fields> | null {
+    return this._search(this.getVonNeumannNeighborhood.bind(this), testFunc, limit);
+  }
+
+  get cellUp() {
+    return this.grid.getCell(this.x, this.y - 1);
+  }
+
+  get cellDown() {
+    return this.grid.getCell(this.x, this.y + 1);
+  }
+
+  get cellLeft() {
+    return this.grid.getCell(this.x - 1, this.y);
+  }
+
+  get cellRight() {
+    return this.grid.getCell(this.x + 1, this.y);
+  }
+
+  distanceTo(cell: Cell<Fields>): number {
+    return Math.sqrt(Math.pow(cell.x - this.x, 2) + Math.pow(cell.y - this.y, 2));
+  }
+
+  /** Allows strict equality checking */
+  [Symbol.toPrimitive](hint: string) {
+    if (hint === 'string') {
+      return this.toString();
+    }
+    if (hint === 'number') {
+      return this.x + this.y * this.grid.width;
+    }
+    return false;
+  }
+
+  toString() {
+    return `Cell(x: ${this.x}, y: ${this.y})`;
+  }
+
+  [Symbol.toStringTag]() {
+    return 'Cell';
+  }
+}
+
+export default class Grid<Fields> {
+  width: number;
+  height: number;
+  fields: INdarrayMap;
+  _cells: { [hash: string]: Cell<Fields> };
+
+  constructor(width: number, height: number, fields: INdarrayMap) {
+    this.width = width;
+    this.height = height;
+    this.fields = fields;
+    this._cells = {};
+  }
+
+  /** Get a cell in the grid, create an instance if it doesn't already exist */
+  getCell(x: number, y: number): Cell<Fields> {
+    const hashKey = `${x},${y}`;
+    if (hashKey in this._cells) {
+      return this._cells[hashKey];
+    }
+    const cell = new Cell(x, y, this);
+    this._cells[hashKey] = cell;
+    return cell;
+  }
+
+  /** Allow iteration over the cells in the grid */
+  [Symbol.iterator] = function *(): Iterator<Cell<Fields>> {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        yield this.getCell(x, y);
+      }
+    }
+  }
+
+  /** Get a field's value on the grid */
+  getField(x: number, y: number, fieldName: string) {
+    return this.fields[fieldName].get(x, y);
+  }
+  
+  /** Set a field on the grid to a value */
+  setField(x: number, y: number, fieldName: string, value: any) {
+    this.fields[fieldName].set(x, y, value);
+  }
+
+  [Symbol.toStringTag]() {
+    return 'Grid';
+  }
+}
+
